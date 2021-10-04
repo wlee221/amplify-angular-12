07dc0492723de11eb8ad424b1af57178a5f17d4d
wlee221 <wlee221@live.unc.edu>
2021-10-04 14:32:34 -0700

    Update amplify example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UiAngularModule } from '@aws-amplify/ui-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UiAngularModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,9 +57,14 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
+import 'zone.js'; // Included with Angular CLI.
+(window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
